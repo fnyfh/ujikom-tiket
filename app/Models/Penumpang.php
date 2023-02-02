b6de12fb1ddaa7ea374b6cbe9dd65492ae6e6858
fnyfh <fnyfh10@gmail.com>
2023-02-02 20:10:15 +0700

    login dan register

---
+++ app/Models/Penumpang.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Petugas extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nama_penumpang',
+        'alamat_penumpang',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'username',
+        'password',
+        'telepon',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+}
